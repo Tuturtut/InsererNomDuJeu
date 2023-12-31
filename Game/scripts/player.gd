a4d6d2d04916bc26ca29d6184f368a59eb783dfa
Tuturtut <arthursimonin88@gmail.com>
2023-12-31 19:28:56 +0100

    Suppression d'un ancien fichier et déplacement du code dans le dossier scripts

--- Game/scripts/player.gd
+++ Game/scripts/player.gd
@@ -1,44 +1,90 @@
-extends RigidBody3D
+extends CharacterBody3D
 
-var mouse_sensitivity := 0.001
-var twist_input := 0.0
-var pitch_input := 0.0
-const SPEED := 5000
+# Player nodes
+@onready var head = $head
+@onready var standing_collision_shape = $Standing_collision_shape
+@onready var crouching_collision_shape = $Crouching_collision_shape
+@onready var ray_cast_3d = $RayCast3D
 
-@onready var twist_pivot := $TwistPivot
-@onready var pitch_pivot := $TwistPivot/PitchPivot
+# Variables
+# Movement Speed variables
+const walking_speed := 10
+const dash_speed := 40
+const crouch_speed:= 7
+var current_speed := walking_speed
 
+# Movement variables
+var jump_velocity := 8
+const lerp_speed := 10.0
+const air_lerp_speed := 2
+var current_lerp_speed := lerp_speed
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
+# Informative variable
+const head_y_position := 1.8
+const crouching_depth := -0.5
+
+# Input variables
+const mouse_sensitivity := 0.05
+var direction = Vector3.ZERO
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	var input := Vector3.ZERO
-	input.x = Input.get_axis("move_left","move_right")
-	input.z = Input.get_axis("move_forward", "move_back")
-	
-	apply_central_force(twist_pivot.basis * input * SPEED * delta)
-	
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(deg_to_rad(-event.relative.x * mouse_sensitivity))		
+		head.rotate_x(deg_to_rad(-event.relative.y * mouse_sensitivity))
+		head.rotation.x = clamp(head.rotation.x, deg_to_rad(-90), deg_to_rad(90))
+
+func _physics_process(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-		
-	twist_pivot.rotate_y(twist_input)
-	pitch_pivot.rotate_x(pitch_input)
-	pitch_pivot.rotation.x = clamp(
-		pitch_pivot.rotation.x,
-		-deg_to_rad(90),
-		deg_to_rad(90)
-	)
 	
-	twist_input = 0.0
-	pitch_input = 0.0
-	
+	# Crouch
+	if Input.is_action_pressed("crouch"):
+		current_speed = crouch_speed
 		
-func _unhandled_input(event):
-	if event is InputEventMouseMotion:
-		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-			twist_input = - event.relative.x * mouse_sensitivity
-			pitch_input = - event.relative.y * mouse_sensitivity
+		standing_collision_shape.disabled = true
+		crouching_collision_shape.disabled = false
+		
+		head.position.y = lerp(head.position.y, head_y_position + crouching_depth, delta * lerp_speed * 2)
+	elif !ray_cast_3d.is_colliding():
+		standing_collision_shape.disabled = false
+		crouching_collision_shape.disabled = true
+		
+		head.position.y = lerp(head.position.y, head_y_position, delta * lerp_speed)
+		
+		# Dash
+		if Input.is_action_pressed("dash"):
+			current_speed = dash_speed
+		else:
+			current_speed = walking_speed
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+		current_lerp_speed = 3
+	else:
+		current_lerp_speed = 15
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = jump_velocity
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	direction = lerp(direction, (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized(), delta * current_lerp_speed)
+	if direction:
+		velocity.x = direction.x * current_speed
+		velocity.z = direction.z * current_speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, current_speed)
+		velocity.z = move_toward(velocity.z, 0, current_speed)
+
+	move_and_slide()
